dcb95ee6f2584099c5e49394068420e5668b0fc6
SkylerHaataja <sdhaataj@ucsc.edu>
2022-04-10 01:04:10 -0700

    update 0.18

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -16,14 +16,26 @@ class Play extends Phaser.Scene {
         /* The add.tileSprite() method expects five parameters: x-position, y-position,
            width, height, and a key string that tells us which image to use. */
         // green UI background
-        this.add.rectangle(0, borderUISize + borderPadding, game.config.width, borderUISize * 2, 0x00FF00).setOrigin(0, 0);
+        
+        this.add.rectangle(0, borderUISize + borderPadding, game.config.width, borderUISize * 2, 0x8080AF).setOrigin(0, 0);
         // white borders
         this.add.rectangle(0, 0, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0, 0);
         this.add.rectangle(0, game.config.height - borderUISize, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0, 0);
         this.add.rectangle(0, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);
         this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);
+        // add rocket (p1)
+        this.p1Rocket = new Rocket(this, game.config.width/2, game.config.height - borderUISize - borderPadding, 'rocket').setOrigin(0.5, 0);
+        // define keys
+        keyF = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
+        keyR = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R);
+        keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
+        keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
+        
     }
     update() {
+        
         this.starfield.tilePositionX -= 2;
+        this.starfield.tilePositionY -= 1;
+        this.p1Rocket.update();
     }
 }
